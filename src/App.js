9e7c2cb843f454d58454e412fef8985751c7373e
Tobi <tobias.eisenschenk@nui.care>
2020-03-21 19:11:29 +0100

    build: add firebase hosting meta

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Covid19 Buddy. A first responder chatbot.
         </p>
         <a
           className="App-link"
